ec898daea04f6357fca4a1b03f9eb0b4b8a122d7
Oliver Sim <160069657+AmericanSpirit38@users.noreply.github.com>
2025-11-10 18:36:30 +0100

    Added linkedin and x links. Also added the first part of the treaure hunt.

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,24 @@
-import { useState } from 'react'
+import React from 'react';
+import { useState } from 'react';
 import githubLogo from './assets/github-icon.svg'
 import instagramLogo from './assets/instagram-icon.svg'
+import linkedinLogo from './assets/linkedin-icon.svg'
+import xLogo from './assets/x-icon.svg'
 
 import './App.css'
 
 function App() {
-
+    const [ButtonPressCount, setButtonPressCount] = useState(0);
   return (
     <>
         <div className="body">
             <div className="name">
                 <button onClick={() => {
-                    alert("Hello! My name is Oliver Sim, and I'm a software developer specializing in web development. Welcome to my portfolio site!")
+                    alert("Hello visitor! Can you find the secret? " + (5 - ButtonPressCount));
+                    setButtonPressCount(c => c + 1);
+                    if (ButtonPressCount + 1 > 5) {
+                        alert("Congratulations! You've found the secret by clicking the name 5 times!");
+                    }
                 }}>
                     <div className="name">
                         Oliver Sim
@@ -27,6 +34,16 @@ function App() {
                 <div className="logo">
                     <a href="https://www.instagram.com/oliver.sim.9/" target="_blank">
                         <img src={instagramLogo} className="logo" alt="linked instagram logo" />
+                    </a>
+                </div>
+                <div className="logo">
+                    <a href="https://www.linkedin.com/in/oliver-sim-978b2b364/" target="_blank">
+                        <img src={linkedinLogo} className="logo" alt="linked linkedIn logo" />
+                    </a>
+                </div>
+                <div className="logo">
+                    <a href="https://x.com/OliverSim10" target="_blank">
+                        <img src={xLogo} className="logo" alt="linked x logo" />
                     </a>
                 </div>
             </div>
